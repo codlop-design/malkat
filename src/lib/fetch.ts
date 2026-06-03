bd91fbbade1bc6619bbebe693a4a4bbc03326d54
ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2026-06-03 14:31:10 +0300

    Remove credentials option from fetcher function to streamline API requests

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -14,7 +14,6 @@ export async function fetcher<T>(
         "Content-Type": "application/json",
         ...options?.headers,
       },
-      credentials: "include",
     });
 
     if (!response.ok) {
